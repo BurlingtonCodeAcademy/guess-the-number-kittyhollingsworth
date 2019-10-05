2f3b883ae21394eef00770961f5c8f38cba192df
kittyhollingsworth <kittyhollingsworth@gmail.com>
2019-10-05 15:06:51 -0400

    added first few functions, highLow function not working

--- index.js
+++ index.js
@@ -7,43 +7,71 @@ function ask(questionText) {
   });
 }
 
+function findMiddlePoint (min,max){
+    let middle = Math.floor((min + max) / 2)
+    return middle;
+}
+async function higherOrLower () {
+  let highLow = await ask('Is it higher (H), or lower (L)?');
+  console.log("pew")
+  
+  if(highLow === 'H'){
+    console.log('high')
+  } else {
+    console.log('low')
+  }
+  console.log('test')
+}
 start();
 
 async function start() {
   console.log("Let's play a game where you (human) make up a number between 1 and 100 and I (computer) try to guess it.")
   let secretNumber = await ask("What is your secret number?\nI won't peek, I promise...\n");
   console.log('You entered: ' + secretNumber);
-  let num = 50;
-  let guess = await ask("is it " + num + "?\n")
+  
+  let min = 1
+  let max = 100
+  let guessnum = findMiddlePoint(min, max);
+  let guess = await ask("is it " + guessnum + "?\n");
+
+
+
+
+//go back and add toLowerCase and no
+//go back and add toLowerCase and yes 
  
- /* if(guess === 'N') {
-    console.log('sorry i was wrong');
-  } 
-*/
-  switch (guess) {
-    case "N": 
-      console.log('derp');
-      break;
-    case "Y":
-      console.log('yay');
-      break;
+  if (guess === 'n') {
+    higherOrLower();
+  } else if (guess.toLowerCase === 'y' || guess.toLowerCase === 'yes') {
+    console.log('Your number was ' + guess)
   }
 
-
-
-  // Now try and complete the program.
   process.exit();
 }
 
-// function takes and finds half way point between two numbers and defines new guess
-function halfWayPoint (guess,){
 
-}
+
+
+
+//  switch (guess) {
+//    case "N": 
+//      console.log('derp');
+//      break;
+//    case "Y":
+//      console.log('yay');
+//      break;
+//  }
+
+
+
+// Now try and complete the program.
+
+
+// function takes and finds half way point between two numbers and defines new guess
+
 // takes new guess and asks if it is correct
 
 // if yes your number is 66
 // if no, is your number higher or lower
 
 //higher takes gue
-
-
